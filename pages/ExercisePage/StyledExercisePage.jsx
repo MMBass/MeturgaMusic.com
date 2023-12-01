5a39bb80dfa8798213f95d9e2b813fdf061dc919
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2023-12-01 18:57:28 +0200

    Delete pages directory

--- pages/ExercisePage/StyledExercisePage.jsx
+++
@@ -1,8 +0,0 @@
-import { styled } from '@mui/material/styles';
-import ExercisePage from './ExercisePage';
-
-const StyledExercisePage = styled(ExercisePage)`
-   min-height: 80vh;
-`;
-
-export default StyledExercisePage;
--- pages/WishlistPage/StyledWishlistPage.jsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/material/styles';
-import WishlistPage from './WishlistPage';
-
-const StyledWishlistPage = styled(WishlistPage)`
-  min-height: 70vh;
-  padding-top: 3px;
-`;
-
-export default StyledWishlistPage;
