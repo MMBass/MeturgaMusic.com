43ffb9e23b3829515afb6a6cd304a8822769080e
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2023-12-05 05:13:43 +0200

    bable config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import babel from "vite-plugin-babel";
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/repo-name', // use if no custom domain
-  plugins: [react()],
+  plugins: [react(), babel()],
   resolve: {
     alias: {
       '@pages': path.resolve(__dirname, './src/pages'),
       '@pages_songs': path.resolve(__dirname, './src/pages_songs'),
       '@components': path.resolve(__dirname, './src/components'),
       '@context': path.resolve(__dirname, './src/context'),
-      '@' : path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
