ca1ad1fb9bfe03e2c1cf1c3377bea92f93e91daa
unknown <mendibass@gmail.com>
2023-12-06 18:55:36 +0200

    Fast deploy. no message

--- src/components/HeadTags/HeadTags.jsx
+++ src/components/HeadTags/HeadTags.jsx
@@ -1,7 +1,7 @@
 //** uses for conditional and dynamic head tags **/
 
 import { useState, useEffect } from 'react';
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
 function HeadTags(props) {
   const [themeColor, setThemeColor] = useState(props.theme.palette.primary.main);
@@ -11,14 +11,19 @@ function HeadTags(props) {
   }, [props.theme]);
 
   return (
-      <Helmet>
-        <title>
-          {props.currTitle}
-        </title>
-        <meta name="theme-color" content={themeColor}/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
-      </Helmet>
+    <Helmet>
+      <title>
+        {props.currTitle}
+      </title>
+      <meta name="theme-color" content={themeColor} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
+      {/* <!-- .BassSites Sarch-console tag --> */}
+      {/* <!-- <meta name="google-site-verification" content="Ovp5O_FlrRV0WVdSIMpbGK4gNeds504xdwOMPXJn7XI" /> --> */}
+
+      {/* <!-- Meturgamusic Sarch-console tag --> */}
+      <meta name="google-site-verification" content="AGB8vajsDH1bMMQGmfSJR01j8pfQfQrHRP8cUxFiZH4" />
+    </Helmet>
   );
 }
 
