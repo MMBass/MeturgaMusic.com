5e5b0a41cbcb7ef089b36a3d588feaf3b073d3c5
unknown <mendibass@gmail.com>
2024-01-03 16:04:10 +0200

    footerLinks comp, i18n to templates

---
+++ src/components/FooterLinks/StyledFooterLinks.jsx
@@ -0,0 +1,8 @@
+import { styled } from '@mui/material/styles';
+import FooterLinks from './FooterLinks';
+
+const StyledFooterLinks = styled(FooterLinks)`
+  
+`;
+
+export default StyledFooterLinks;
