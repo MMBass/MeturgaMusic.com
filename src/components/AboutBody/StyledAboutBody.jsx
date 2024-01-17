431ed7ac96add85a8458140228dc8225d6aa0336
unknown <mendibass@gmail.com>
2024-01-17 21:37:32 +0200

    new about mobile colors

--- src/components/AboutBody/StyledAboutBody.jsx
+++ src/components/AboutBody/StyledAboutBody.jsx
@@ -3,8 +3,8 @@ import AboutBody from './AboutBody';
 
 const StyledAboutBody = styled(AboutBody)`
   width: 100%;
-  margin: 0 0 60px 0;
   font-size: 2vw;
+  padding-top: 50px;
   font-family: 'Assistant','Roboto', 'sans-serif';
   text-align: center;
 
@@ -12,28 +12,49 @@ const StyledAboutBody = styled(AboutBody)`
     font-size: 4.5vw;
   }
 
-  .even-item{
-    flex-direction: row-reverse;
-
-    .strings-item{
-      text-align: end;
-      @media (max-width: 600px) {
-        text-align: start;
-      }
-    }
-  }
-
   .even-item, .not-even-item{
-    padding-top: 25px;
+    margin: 5px 0;
+    margin-bottom: 30px;
     display: flex;
     align-items: center;
     background-color: ${props => props.theme.palette.secondary.light};
+
+    @media (max-width: 600px) {
+      background-color: antiquewhite;
+      text-align: center;
+      margin-bottom: 0px;
+      box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px; 
+    }
+  }
+  
+  .even-item{
+    flex-direction: row-reverse;
+    @media (max-width: 600px) {
+      background-color: antiquewhite;
+      .strings-item{
+        text-align: center;
+      }
+    }
+
+    .strings-item{
+      text-align: end;
+    }
+  }
+
+  .not-even-item{
+    @media (max-width: 600px) {
+      background-color: aliceblue;
+      .strings-item{
+        text-align: center;
+      }
+    }
   }
 
   .about-body-top{
     text-align: start;
 
     @media (max-width: 600px) {
+      margin-top: 30px;
       height: 30px;
     }
 
@@ -49,9 +70,9 @@ const StyledAboutBody = styled(AboutBody)`
   }
   
   .strings-item{
-     text-align: start;
+    text-align: start;
     @media (max-width: 600px) {
-      padding: 8px 40px 40px 40px;
+      padding: 8px 40px 70px 40px;
     }
 
     .about-header{
@@ -96,15 +117,18 @@ const StyledAboutBody = styled(AboutBody)`
       display: flex;
       justify-content: center;
       height: 100%;
+
       background: radial-gradient(circle, #edcbcb 60%, rgb(223, 128, 142) 100%);
       border-radius: 13px;
 
       @media (max-width: 600px) {
+        background: unset;
         padding: 15px 4px;
       }
     }
 
     img{
+      /* filter: invert(100%); // use when dark mode */
       margin: auto;
       width: 95%;
       height: auto;
@@ -113,6 +137,7 @@ const StyledAboutBody = styled(AboutBody)`
         width: 99%;
         height: auto;
         max-height: 90%;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
         /* max-height: 200px; */
       }
     }
