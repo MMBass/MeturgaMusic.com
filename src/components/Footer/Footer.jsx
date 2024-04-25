68b01dfea41de54ba5120f6125ffc87396e4624d
unknown <mendibass@gmail.com>
2024-04-25 21:05:12 +0300

    display the OfferInstall

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,15 +5,13 @@ import utils from '@/utils';
 function Footer({ className }) {
   return (
     <>
-
-    {/* Display the footer links for not  installed version*/}
+    {/* Display the footer links for not installed version*/}
       {!window.matchMedia('(display-mode: standalone)').matches &&
         <div id="FOOTER" className={className} >
           <FooterLinks
-            canInstall={(utils.isApple())}></FooterLinks>
+            canInstall={(!utils.isApple())}></FooterLinks>
         </div>
       }
-      
     </>
   );
 }
