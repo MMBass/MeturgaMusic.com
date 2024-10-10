96210fd2961a96a59280fa95aaae678fe361487e
unknown <mendibass@gmail.com>
2024-10-10 12:58:22 +0300

    i18n 3 changes

--- src/components/OfferInstall/OfferInstallI18n.js
+++ src/components/OfferInstall/OfferInstallI18n.js
@@ -1,6 +1,6 @@
 export default {
     Header: "התקן את מתורגמיוזיק כאפליקציה",
-    Description: "(אינו נתמך בכל המכשירים)",
+    Description: "(אינו נתמך בכל סוגי המכשירים)",
     Install: "התקן",
     CantInstallAlert: "כבר הותקן, או שמכשירך אינו תומך בהתקנה",
 };
