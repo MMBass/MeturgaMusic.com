6aafe92e478a8abba9e829e8c51f7a93bc555b43
unknown <mendibass@gmail.com>
2025-05-25 14:28:43 +0300

    start of
    LyricsBodyTopActions comp

---
+++ src/components/LyricsBodyTopActions/StyledLyricsBodyTopActions.jsx
@@ -0,0 +1,8 @@
+import { styled } from '@mui/material/styles';
+import LyricsBodyTopActions from './LyricsBodyTopActions';
+
+const StyledLyricsBodyTopActions = styled(LyricsBodyTopActions)`
+  
+`;
+
+export default StyledLyricsBodyTopActions;
