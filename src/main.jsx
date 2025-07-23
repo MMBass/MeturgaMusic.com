999767348896f89a23d03695e11590c10dd66889
unknown <mendibass@gmail.com>
2025-07-23 19:11:22 +0300

    initIdFromUrl added

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,18 @@ import ContextComposer from '@context/ContextComposer';
 import { LOCAL_STORAGE_KEYS } from '@/constants';
 
 import { v4 as uuidv4 } from 'uuid';
+const urlParams = new URLSearchParams(window.location.search);
+const initIdFromUrl = urlParams.get('INITID');
 
-if (!localStorage.getItem(LOCAL_STORAGE_KEYS.INIT)) localStorage.setItem(LOCAL_STORAGE_KEYS.INIT, 'INITID:' + uuidv4());
+if (!localStorage.getItem(LOCAL_STORAGE_KEYS.INIT)) {
+  if (initIdFromUrl?.length === 36 /* UUID length */) localStorage.setItem(LOCAL_STORAGE_KEYS.INIT, initIdFromUrl);
+  else localStorage.setItem(LOCAL_STORAGE_KEYS.INIT, 'INITID:' + uuidv4());
+}
 
 ReactDOM.render(
   <StrictMode>
     <ContextComposer>
-      <App className='app'/>
+      <App className='app' />
     </ContextComposer>
   </StrictMode>,
   document.getElementById('root')
