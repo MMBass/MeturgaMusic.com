00a9cc7a8678407fec61f43a7e35be6a8e86ab8b
unknown <mendibass@gmail.com>
2025-11-06 08:32:44 +0200

    adsense ads spacing pt2

--- src/components/AdsenseMediaWebAdd/StyledAdsenseMediaWebAdd.jsx
+++ src/components/AdsenseMediaWebAdd/StyledAdsenseMediaWebAdd.jsx
@@ -2,11 +2,7 @@ import { styled } from '@mui/material/styles';
 import AdsenseMediaWebAdd from './AdsenseMediaWebAdd';
 
 const StyledAdsenseMediaWebAdd = styled(AdsenseMediaWebAdd)`
-    padding: '5px';
 
-    @media (max-width: 600px) {
-        padding: '2px';
-    }
 `;
 
 export default StyledAdsenseMediaWebAdd;
