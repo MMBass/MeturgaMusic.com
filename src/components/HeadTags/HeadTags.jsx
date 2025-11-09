20a92a01a683fbd3723c71e473166aea4e04e647
unknown <mendibass@gmail.com>
2025-11-09 19:11:01 +0200

    fix: adsense tag move to helmet

--- src/components/HeadTags/HeadTags.jsx
+++ src/components/HeadTags/HeadTags.jsx
@@ -13,6 +13,12 @@ function HeadTags({ currTitle }) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
+
+      {/* Google adsense global tag */}
+      {currTitle &&
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8294214228053744"
+          crossorigin="anonymous" data-overlays="bottom" ></script>
+      }
     </Helmet>
   );
 }
