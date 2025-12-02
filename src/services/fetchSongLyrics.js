2eb02106865d7fce1923463356cc6b7c358d8d48
unknown <mendibass@gmail.com>
2025-12-02 04:04:20 +0200

    fix/bckp server for lyrics fetch

--- src/services/fetchSongLyrics.js
+++ src/services/fetchSongLyrics.js
@@ -3,18 +3,27 @@ import utils from '@/utils.js';
 
 export default async (splittedSongTitle, webSongUrl) => {
     const initId = utils.isLocalhost() ? "localhost" : localStorage.getItem(LOCAL_STORAGE_KEYS.INIT);
-    
-    const response = await fetch(`${URLS.PROD_SERVER_URL}/lyrics?initId=` + initId, {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "currSong": splittedSongTitle,
-            "webSongUrl": webSongUrl
-        })
-    });
+
+    const innerFetch = async (currServer) => {
+        return await fetch(`${currServer}/lyrics?initId=` + initId, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "currSong": splittedSongTitle,
+                "webSongUrl": webSongUrl
+            })
+        });
+    }
+
+    let response = null;
+    try {
+        response = await innerFetch(URLS.PROD_SERVER_URL);
+    } catch {
+        response = await innerFetch(URLS.PROD_BCKP_SERVER_URL);
+    }
 
     return response.json();
 }
