2f510f4f6d945c8f5497221a5ce059ca102369af
unknown <mendibass@gmail.com>
2026-01-21 07:05:14 +0200

    /lyrics path to /lyrics/search

--- src/services/fetchSongLyrics.js
+++ src/services/fetchSongLyrics.js
@@ -5,40 +5,17 @@ export default async (splittedSongTitle, webSongUrl) => {
     const initId = utils.isLocalhost() ? "localhost" : localStorage.getItem(LOCAL_STORAGE_KEYS.INIT);
 
     const callSearch = async (currServer) => {
-        try {
-            // move to catch until block done:
-            // throw;
-            
-            // client side call webSongUrl
-            const htmlSong = await fetch('https://genius.com/Sabrina-carpenter-espresso-lyrics').then(response => response.text());
-            console.log(htmlSong);
-           
-            // post to server to process
-            return await fetch(`${currServer}/lyrics/generate?initId=` + initId, {
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "htmlSong": htmlSong,
-                    "currSong": splittedSongTitle,
-                    "webSongUrl": webSongUrl
-                })
-            });
-        } catch {
-            return await fetch(`${currServer}/lyrics/search?initId=` + initId, {
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "currSong": splittedSongTitle,
-                    "webSongUrl": webSongUrl
-                })
-            });
-        }
+        return await fetch(`${currServer}/lyrics/search?initId=` + initId, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "currSong": splittedSongTitle,
+                "webSongUrl": webSongUrl
+            })
+        });
     }
 
     let response = null;
