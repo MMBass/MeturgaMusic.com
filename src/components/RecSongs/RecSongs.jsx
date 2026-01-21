c65c15a00ea017eb46b53bec26d838d7097119ac
unknown <mendibass@gmail.com>
2026-01-21 08:19:48 +0200

    RecSongs chips style

--- src/components/RecSongs/RecSongs.jsx
+++ src/components/RecSongs/RecSongs.jsx
@@ -6,9 +6,9 @@ import Grid from '@mui/material/Grid';
 
 const recSongsData = [
     { artist: 'Alex Warren', title: 'Ordinary', url: 'https://meturgamusic.com/?song=Alex-Warren_Ordinary' },
-    { artist: 'RAYE', title: 'Where Is My Husband', url: 'https://meturgamusic.com/?song=RAYE_WHERE-IS-MY-HUSBAND%21' },
     { artist: 'Teddy Swims', title: 'Lose Control', url: 'https://meturgamusic.com/?song=Teddy-Swims_Lose-Control' },
     { artist: 'Olivia Dean', title: 'So Easy (To Fall In Love)', url: 'https://meturgamusic.com/?song=Olivia-Dean_So-Easy-To-Fall-In-Love' },
+    { artist: 'RAYE', title: 'Where Is My Husband', url: 'https://meturgamusic.com/?song=RAYE_WHERE-IS-MY-HUSBAND%21' },
     { artist: 'Brick + Mortar', title: 'Brighter Than The Sun', url: 'https://meturgamusic.com/?song=Brick-%2B-Mortar_Brighter-Than-The-Sun' },
     { artist: 'Saint Motel', title: 'Hold My Place In Heaven', url: 'https://meturgamusic.com/?song=Saint-Motel_Hold-My-Place-In-Heaven' },
 ];
@@ -23,7 +23,7 @@ const RecSongs = ({ className }) => {
                         <Chip
                             label={`${song.artist} - ${song.title}`}
                             clickable
-                            //  size='small' 
+                            size='medium' 
                         />
                     </a>
                     </Grid>
