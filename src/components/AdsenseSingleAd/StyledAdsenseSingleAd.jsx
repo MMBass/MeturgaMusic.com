6ad7eb688127adb9ad281e24cbc6a26aa8537a5d
unknown <mendibass@gmail.com>
2026-07-09 14:09:14 +0300

    fix: AdsenseSingleAd fix style

--- src/components/AdsenseSingleAd/StyledAdsenseSingleAd.jsx
+++ src/components/AdsenseSingleAd/StyledAdsenseSingleAd.jsx
@@ -2,9 +2,8 @@ import { styled } from '@mui/material/styles';
 import AdsenseSingleAd from './AdsenseSingleAd';
 
 const StyledAdsenseSingleAd = styled(AdsenseSingleAd)`
-      border-radius: 10px;
-      margin: 0px 10px 20px 15px;
-      padding: 5px;
+      border-radius: 25px;
+      padding: 25px;
       background: #f1f1f1d9;
 `;
 
