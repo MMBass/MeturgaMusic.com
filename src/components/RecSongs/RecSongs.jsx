a17d80f3bd1f4fbec8f5e6e6c9c6e595462f8665
unknown <mendibass@gmail.com>
2026-07-29 13:17:30 +0300

    fix: recSongsData hrefs song= to /songs

--- src/components/RecSongs/RecSongs.jsx
+++ src/components/RecSongs/RecSongs.jsx
@@ -4,12 +4,12 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
 const recSongsData = [
-    { artist: 'Alex Warren', title: 'Ordinary', url: 'https://meturgamusic.com/?song=Alex-Warren_Ordinary' },
-    { artist: 'Teddy Swims', title: 'Lose Control', url: 'https://meturgamusic.com/?song=Teddy-Swims_Lose-Control' },
-    { artist: 'Olivia Dean', title: 'So Easy (To Fall In Love)', url: 'https://meturgamusic.com/?song=Olivia-Dean_So-Easy-To-Fall-In-Love' },
-    { artist: 'RAYE', title: 'Where Is My Husband', url: 'https://meturgamusic.com/?song=RAYE_WHERE-IS-MY-HUSBAND%21' },
-    { artist: 'Brick + Mortar', title: 'Brighter Than The Sun', url: 'https://meturgamusic.com/?song=Brick-%2B-Mortar_Brighter-Than-The-Sun' },
-    { artist: 'Saint Motel', title: 'Hold My Place In Heaven', url: 'https://meturgamusic.com/?song=Saint-Motel_Hold-My-Place-In-Heaven' },
+    { artist: 'Alex Warren', title: 'Ordinary', url: 'https://meturgamusic.com/songs/Alex-Warren_Ordinary' },
+    { artist: 'Teddy Swims', title: 'Lose Control', url: 'https://meturgamusic.com/songs/Teddy-Swims_Lose-Control' },
+    { artist: 'Olivia Dean', title: 'So Easy (To Fall In Love)', url: 'https://meturgamusic.com/songs/Olivia-Dean_So-Easy-To-Fall-In-Love' },
+    { artist: 'RAYE', title: 'Where Is My Husband', url: 'https://meturgamusic.com/songs/RAYE_WHERE-IS-MY-HUSBAND%21' },
+    { artist: 'Brick + Mortar', title: 'Brighter Than The Sun', url: 'https://meturgamusic.com/songs/Brick-%2B-Mortar_Brighter-Than-The-Sun' },
+    { artist: 'Saint Motel', title: 'Hold My Place In Heaven', url: 'https://meturgamusic.com/songs/Saint-Motel_Hold-My-Place-In-Heaven' },
 ];
 
 const RecSongs = ({ className }) => {
